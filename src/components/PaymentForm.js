32f2b812741cd982c1d455df1907f6bb1fd3231c
Andrea Fuentes <andrea.fuentes@payulatam.com>
2020-02-15 22:21:57 -0500

    implementing sale

--- src/components/PaymentForm.js
+++ src/components/PaymentForm.js
@@ -2,68 +2,172 @@ import React from "react";
 import Cards from "react-credit-cards";
 import "react-credit-cards/es/styles-compiled.css";
 import logoProvider from "../images/payuLogo.png";
-
+import { Container } from "react-bootstrap";
+import PaymentService from "../services/PaymentService";
+import Select from "react-select";
+import NumericInput from "react-numeric-input";
 export default class PaymentForm extends React.Component {
   state = {
     cvc: "",
     expiry: "",
     focus: "",
     name: "",
-    number: ""
+    number: "",
+    creditType: "",
+    installmentsNumber: 1,
+    saleProducts: []
   };
 
   handleInputFocus = e => {
-    this.setState({ focus: e.target.name });
+    if (e.tar) this.setState({ focus: e.target.name });
+  };
+
+  handleNumericInputChange = event => {
+    this.setState({ installmentsNumber: event });
+  };
+
+  handleSelectChange = event => {
+    this.setState({ creditType: event, creditTypeValue: event.value });
   };
 
   handleInputChange = e => {
-    const { name, value } = e.target;
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
 
-    this.setState({ [name]: value });
+  saleProductList() {
+    this.props.productCartSale.map(productSale => {
+      let saleProduct = {
+        unitPrice: productSale.unitPrice,
+        quantity: productSale.quantity,
+        product: {
+          id: productSale.id
+        }
+      };
+      this.setState({
+        saleProducts: this.state.saleProducts.push(saleProduct)
+      });
+    });
+  }
+
+  handlePay = event => {
+    this.saleProductList();
+    let request = {
+      sale: {
+        shippingAddress: "Calle falsa 123, Bogotaet",
+        saleProducts: this.state.saleProducts,
+        customer: {
+          id: 1
+        }
+      },
+      paymentInfo: {
+        creditCardNumber: this.state.number,
+        creditCardCVV: this.state.cvc,
+        creditCardExpirationDate: this.state.expiry,
+        paymentMethod: this.state.creditType.value,
+        installmentsNumber: this.state.installmentsNumber
+      }
+    };
+
+    const response = PaymentService.post(request);
+    console.log(response);
   };
 
   render() {
-    return (
-      <div id="PaymentForm">
-        <Cards
-          cvc={this.state.cvc}
-          expiry={this.state.expiry}
-          focus={this.state.focus}
-          name={this.state.name}
-          number={this.state.number}
-        />
-        <form>
-          <input
-            type="tel"
-            name="number"
-            placeholder="Card Number"
-            onChange={this.handleInputChange}
-            onFocus={this.handleInputFocus}
-          />
-          <input
-            type="text"
-            name="name"
-            placeholder="Name"
-            onChange={this.handleInputChange}
-            onFocus={this.handleInputFocus}
-          />
-          <input
-            type="tel"
-            name="cvc"
-            placeholder="CVC"
-            onChange={this.handleInputChange}
-            onFocus={this.handleInputFocus}
-          />
+    const options = [
+      { value: "VISA", label: "Visa" },
+      { value: "MASTERCARD", label: "Mastercard" }
+    ];
 
-          <input
-            type="text"
-            name="expiry"
-            placeholder="yyyy/mm"
-            pattern="[2][0][2-9][0-9]\/([0][1-9]|[1][1-2])"
-          />
-          <img src={logoProvider} alt="Logo provider" />
-        </form>
-      </div>
+    return (
+      <Container>
+        <div id="PaymentForm">
+          <Container>
+            <Cards
+              cvc={this.state.cvc}
+              expiry={this.state.expiry}
+              focus={this.state.focus}
+              name={this.state.name}
+              number={this.state.number}
+            />
+          </Container>
+          <form>
+            <label>Card type:</label>
+            <Container>
+              <Select
+                placeholder="Select your credit card type"
+                value={this.state.creditType}
+                options={options}
+                onChange={this.handleSelectChange}
+                onFocus={this.handleInputFocus}
+              />
+            </Container>
+            <label>Card Number:</label>
+            <Container>
+              <input
+                type="tel"
+                name="number"
+                placeholder="1111222233334444"
+                onChange={this.handleInputChange}
+                onFocus={this.handleInputFocus}
+              />
+            </Container>
+            <label>Name:</label>
+            <Container>
+              <input
+                type="text"
+                name="name"
+                placeholder="Pepe Perez"
+                onChange={this.handleInputChange}
+                onFocus={this.handleInputFocus}
+              />
+            </Container>
+            <label>Segurity number:</label>
+            <Container>
+              <input
+                type="tel"
+                name="cvc"
+                placeholder="123"
+                onChange={this.handleInputChange}
+                onFocus={this.handleInputFocus}
+              />
+            </Container>
+            <label>Expiration date:</label>
+            <Container>
+              <input
+                type="text"
+                name="expiry"
+                placeholder="yyyy/mm"
+                pattern="[2][0][2-9][0-9]\/([0][1-9]|[1][1-2])"
+                onChange={this.handleInputChange}
+                onFocus={this.handleInputFocus}
+              />
+            </Container>
+
+            <label>installmentsNumber:</label>
+            <Container>
+              <NumericInput
+                name="installmentsNumber"
+                value={this.state.installmentsNumber}
+                step={1}
+                precision={0}
+                size={3}
+                min={1}
+                max={12}
+                onChange={this.handleNumericInputChange}
+              />
+            </Container>
+
+            <Container>
+              <img src={logoProvider} alt="Logo provider" />
+            </Container>
+          </form>
+          <button onClick={this.handlePay} className="btn btn-primary">
+            Pay
+          </button>
+        </div>
+      </Container>
     );
   }
 }
--- src/pages/ShoppingCart.js
+++ src/pages/ShoppingCart.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
-import PaymentService from "../services/PaymentService";
+
 import PaymentForm from "../components/PaymentForm";
 import { Table, Thead, Tbody, Tr, Th, Td } from "react-super-responsive-table";
 import "react-super-responsive-table/dist/SuperResponsiveTableStyle.css";
+import { Container } from "react-bootstrap";
 
 export default class ShoppingCart extends Component {
   state = {
@@ -10,20 +11,16 @@ export default class ShoppingCart extends Component {
     totalPrice: 0
   };
 
-  handlePay = event => {
-    let request = {};
-    const response = PaymentService.post(request);
-    console.log(response);
-  };
-
   componentDidMount() {
     let shoppingCart = JSON.parse(localStorage.getItem("shoppingCart"));
 
-    let calcTotalPrice = 0;
-    this.setState({
-      productsSale: shoppingCart,
-      totalPrice: calcTotalPrice
-    });
+    if (shoppingCart != null) {
+      let calcTotalPrice = 0;
+      this.setState({
+        productsSale: shoppingCart,
+        totalPrice: calcTotalPrice
+      });
+    }
   }
 
   render() {
@@ -32,32 +29,39 @@ export default class ShoppingCart extends Component {
         {console.log("this.state.productsSale")}
         {console.log(this.state.productsSale)}
         <h2>Sale</h2>
-        <Table>
-          <Thead>
-            <Tr>
-              <Th>name</Th>
-              <Th>description</Th>
-              <Th>quantity</Th>
-              <Th>unitPrice</Th>
-            </Tr>
-          </Thead>
-          {this.state.productsSale.map(productSale => {
-            return (
-              <Tbody>
-                <Tr>
-                  <Td>{productSale.name}</Td>
-                  <Td>{productSale.description}</Td>
-                  <Td>{productSale.quantity} </Td>
-                  <Td>{productSale.unitPrice}</Td>
-                </Tr>
-              </Tbody>
-            );
-          })}
-        </Table>
-        <button onClick={this.handlePay} className="btn btn-primary">
-          Pay
-        </button>
-        <PaymentForm />
+        <Container>
+          <Table>
+            <Thead>
+              <Tr>
+                <Th>name</Th>
+                <Th>description</Th>
+                <Th>quantity</Th>
+                <Th>unitPrice</Th>
+                <Th>subTotalPrice</Th>
+              </Tr>
+            </Thead>
+            {this.state.productsSale.map(productSale => {
+              return (
+                <Tbody>
+                  <Tr>
+                    <Td>{productSale.name}</Td>
+                    <Td>{productSale.description}</Td>
+                    <Td>{productSale.quantity} </Td>
+                    <Td>{productSale.unitPrice}</Td>
+                    <Td>{productSale.subTotalPrice}</Td>
+                  </Tr>
+                </Tbody>
+              );
+            })}
+          </Table>
+        </Container>
+        <Container>
+          <h1>- -</h1>
+          <h2>Payment Form</h2>
+        </Container>
+        <Container>
+          <PaymentForm productCartSale={this.state.productsSale} />
+        </Container>
       </div>
     );
   }
